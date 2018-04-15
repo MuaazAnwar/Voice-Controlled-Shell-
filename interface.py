d899456a9c1dc28aedb4ac66dc7bd0adc72ad254
nabeelfarooqui98 <nabeelfarooqui98@yahoo.com>
2018-04-15 15:32:32 +0500

    connected buttons to start and exit
    line # 68,69 par connection hai.
    interface.ui (editable in qt designer) and interface.py (pyuic -x -o ka
    result) dono bhi sunc krdiye hian. matlab interface etc change karingay
    to wo bhi synced rahega.
    
    bahot sari confusions hain, but pata nai kisi tarhan chal gaya (crash
    bhi hogaya tha do bar use krne se)

--- interface.py
+++ interface.py
@@ -1,40 +1,12 @@
-#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-# NOTE: this example requires PyAudio because it uses the Microphone class
-
-import speech_recognition as sr
-from appJar import gui
-from playsound import playsound
-from gtts import gTTS
+# Form implementation generated from reading ui file '.\interface.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-
-def press():
-
-    # obtain audio from the microphone
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-
-        print("Say something!")
-        audio = r.listen(source)
-        print("done listening")
-
-    # recognize speech using Google Speech Recognition
-    try:
-        # for testing purposes, we're just using the default API key
-        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-        # instead of `r.recognize_google(audio)`
-        string = r.recognize_google(audio)
-        print("Google Speech Recognition thinks you said " + string)
-
-        tts = gTTS(text=string, lang='en')
-        tts.save("hello.mp3")
-        playsound('hello.mp3')
-
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
@@ -57,9 +29,7 @@ class Ui_Dialog(object):
         self.textBox.setObjectName("textBox")
 
         self.retranslateUi(Dialog)
-        #self.startBTN.released.connect(self.textBox.setText(self,"say something"));
-        self.startBTN.released.connect(press);
-        self.exitBTN.released.connect(exit);
+        self.startBTN.released.connect(self.textBox.backward)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
@@ -73,8 +43,8 @@ class Ui_Dialog(object):
 "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:16pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Press the start button to begin</p></body></html>"))
 
+
 if __name__ == "__main__":
-    print("starting app")
     import sys
     app = QtWidgets.QApplication(sys.argv)
     Dialog = QtWidgets.QDialog()
@@ -82,13 +52,4 @@ if __name__ == "__main__":
     ui.setupUi(Dialog)
     Dialog.show()
     sys.exit(app.exec_())
-    print("ending app")
 
-
-
-
-
-
-# if 'copy' in string:
-#     print("found copy")
-#     commond="cp"
